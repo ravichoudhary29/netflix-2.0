bbd0d79904f5f0d1fd02cb5a5673859c043a59f3
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-04-02 05:44:19 +0530

    Enhancement: Create the Edit profile

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Nav.css";
+import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 function Nav() {
   const [show, handleshow] = useState(false);
+  const navigate = useNavigate();
+
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
       handleshow(true);
@@ -20,11 +23,13 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
+          onClick={() => navigate("/")}
           className="nav__logo"
-          src="https://toppng.com/uploads/preview/netflix-logo-png-download-11660600814vkfgjh8swu.png"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="netflix-logo"
         />
         <img
+          onClick={() => navigate("/profile")}
           className="nav__avatar"
           src="https://www.w3schools.com/howto/img_avatar.png"
           alt="avatar-image"
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import Nav from "../Nav";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="profileScreen">
-      <h1>This is the profile</h1>
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://www.w3schools.com/howto/img_avatar.png"
+            alt="avatar-image"
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className=" profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
