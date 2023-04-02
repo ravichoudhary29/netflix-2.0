621f00ffa5c889ef669c4b7939fce7ea45ab64b6
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-04-02 15:49:26 +0530

    Final: Final build ready with complete stripe integration

--- src/Nav.js
+++ src/Nav.js
@@ -25,7 +25,7 @@ function Nav() {
         <img
           onClick={() => navigate("/")}
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
           alt="netflix-logo"
         />
         <img
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -9,7 +9,7 @@ function LoginScreen() {
       <div className="loginScreen__background">
         <img
           className="loginScreen__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
           alt="loginScreen-Background"
         />
         <button className="loginScreen__button" onClick={() => setSignIn(true)}>
